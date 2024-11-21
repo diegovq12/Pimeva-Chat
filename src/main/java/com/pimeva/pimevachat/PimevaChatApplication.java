801db6d6e0f4738c998d696b77f1b996ee934f93
diegod <diegovq12@gmail.com>
2024-11-21 10:20:04 -0800

    Creacion del proyecto

---
+++ src/main/java/com/pimeva/pimevachat/PimevaChatApplication.java
@@ -0,0 +1,13 @@
+package com.pimeva.pimevachat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PimevaChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PimevaChatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pimeva/pimevachat/PimevaChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pimeva.pimevachat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PimevaChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
